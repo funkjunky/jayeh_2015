a05a09313ca43fd4c47a264a9557e45fb61de136
Jason McCarrell <jason.abz@gmail.com>
2016-10-12 21:45:09 -0400

    Login with oauth probably works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({ login, push }) => (
         <input type="text" name="username" />
         <input type="password" name="password" />
         <input type="submit" value="Login" />
-        <button type="button" onClick={ () => push('/api/auth/google') } style={{backgroundImage: 'url("/dist/googlelogin.png")', width: 200, height: 40, backgroundSize: '100%', display: 'block'}} />
+        <button type="button" onClick={ () => window.location = '/api/auth/google' } style={{backgroundImage: 'url("/dist/googlelogin.png")', width: 200, height: 40, backgroundSize: '100%', display: 'block'}} />
     </form>
 );
 //window.location.replace('/api/auth/google');
