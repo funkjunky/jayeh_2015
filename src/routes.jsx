79b757880b66400e1d1b26b665a76dc0619baf22
Jason McCarrell <jason.abz@gmail.com>
2016-10-14 13:07:13 -0400

    added back header, through routes.

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,6 +8,7 @@ import fetchFullArticle from './component-fetchers/fetchFullArticle.jsx';
 import Login from './components/Login.jsx';
 import fetchUserPanel from './component-fetchers/fetchUserPanel.jsx';
 import Layout from './components/Layout.jsx';
+import Header from './components/Header.jsx';
 
 import { reconnect } from './actions/User.jsx';
 
@@ -17,16 +18,18 @@ var Routes = (store) => {
         //Note: this means we won't load server side page until user is loaded... no matter which page we load...
         //TODO: we don't want to wait for user... we need to not do this on the server side. I think...
         <Route path="/" component={ Layout } onEnter={ (nextState, cb) => store.dispatch(reconnect()) }>
-            <IndexRoute getComponent={ (nextState, cb) => cb(null, fetchBlog(store.dispatch, store.getState().app.loaded)) } />
-            <Route path="/blog" getComponent={ (nextState, cb) => cb(null, fetchBlog(store.dispatch, store.getState().app.loaded)) } />
-            <Route path="/article/create" component={ NewArticle } />
-            <Route path="/article/edit(/:id)" component={ NewArticle } />
+            <Route component={ Header }>
+                <IndexRoute getComponent={ (nextState, cb) => cb(null, fetchBlog(store.dispatch, store.getState().app.loaded)) } />
+                <Route path="/blog" getComponent={ (nextState, cb) => cb(null, fetchBlog(store.dispatch, store.getState().app.loaded)) } />
+                <Route path="/user/:username" getComponents={ ({ params }, cb) => cb(null, fetchUserPanel(store.dispatch, params.username, store.getState().app.loaded)) } />
+                <Route path="/user" getComponents={ ({ params }, cb) => cb(null, fetchUserPanel(store.dispatch, null, store.getState().app.loaded)) } />
+                <Route path="/login" component={ Login } />
+                <Route path="/article/create" component={ NewArticle } />
+                <Route path="/article/edit(/:id)" component={ NewArticle } />
+            </Route>
+
             <Route path="/article/id/:_id" getComponents={ ({ params }, cb) => cb(null, fetchFullArticle(store.dispatch, params, store.getState().app.loaded)) } />
             <Route path="/article/t/:title" getComponent={ ({ params }, cb) => cb(null, fetchFullArticle(store.dispatch, params, store.getState().app.loaded)) } />
-            <Route path="/user/:username" getComponents={ ({ params }, cb) => cb(null, fetchUserPanel(store.dispatch, params.username, store.getState().app.loaded)) } />
-            <Route path="/user" getComponents={ ({ params }, cb) => cb(null, fetchUserPanel(store.dispatch, null, store.getState().app.loaded)) } />
-
-            <Route path="/login" component={ Login } />
         </Route>
     );
 };
