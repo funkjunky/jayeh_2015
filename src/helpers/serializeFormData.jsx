73b846309e5997aa0f9c13a231c1128e68b57b2d
Jason McCarrell <jason.abz@gmail.com>
2016-10-19 11:34:38 -0400

    non string values serializeFormData

--- src/helpers/serializeFormData.jsx
+++ src/helpers/serializeFormData.jsx
@@ -1,7 +1,17 @@
 const serializeFormData = (formData) => {
     let obj = {};
-    for(let [key, val] of formData.entries())
-        obj[key] = val;
+    for(let [key, val] of formData.entries()) {
+        switch(val) {
+            case 'false':
+                obj[key] = false; break;
+            case 'null':
+                obj[key] = null; break;
+            case 'undefined':
+                obj[key] = undefined; break;
+            default:
+                obj[key] = val;
+        }
+    }
 
     return obj;
 };
