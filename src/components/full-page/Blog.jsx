7b8613cdfb806818ca0b7986459bf5c0e977a9c9
Jason McCarrell <jason.abz@gmail.com>
2016-10-20 17:30:52 -0400

    updated ordering for blog articles

--- src/components/full-page/Blog.jsx
+++ src/components/full-page/Blog.jsx
@@ -8,7 +8,7 @@ const Blog = ({ articles }) => (
     <div>
         <Title render={(prevTitle) => prevTitle + ' - Blog'} />
         <span style={{fontFamily: 'Baskerville', fontSize: 36, paddingRight: 10, paddingLeft: 10, borderRight: "solid 2px grey", borderBottom: "solid 2px grey"}}>Blog</span>
-        {articles.map(function(article) {
+        {articles.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1).map(function(article) {
             return (
                 <div key={article._id} style={{marginLeft: '5%', maxWidth: 800, height: 120}}>
                     <ArticleSummary article={article} />
