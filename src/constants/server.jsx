dd3f3ed6532ae2c8eb3db8665d132e58fa649f7b
Jason McCarrell <jason.abz@gmail.com>
2016-10-23 16:45:59 -0400

    typo

--- src/constants/server.jsx
+++ src/constants/server.jsx
@@ -1,3 +1,3 @@
 //TODO: fuck Node. If it accepted relative URLs then I'd have no issue... instead I need to do this garbage.
 //Or pass fetch through a fucking chain giving it the parameters from req.
-export const host = "http://wwwjayeh.ca";
+export const host = "http://www.jayeh.ca";
