f95d4b7f365cc0fe79931bc183ea54d15ee90cf4
Jason McCarrell <jason.abz@gmail.com>
2016-10-23 16:23:25 -0400

    changed host to http://www.jayeh.ca

--- src/constants/server.jsx
+++ src/constants/server.jsx
@@ -1,3 +1,3 @@
 //TODO: fuck Node. If it accepted relative URLs then I'd have no issue... instead I need to do this garbage.
 //Or pass fetch through a fucking chain giving it the parameters from req.
-export const host = "https://jayeh2015.herokuapp.com";
+export const host = "http://wwwjayeh.ca";
